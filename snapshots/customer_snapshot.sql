c32061a99f66eea19b56b593a132802873c3b9dc
Roshan Sayyad <roshan.syed20@gmail.com>
2026-05-20 19:23:45 +0530

    Add customer_snapshot.sql for customer data snapshot

---
+++ snapshots/customer_snapshot.sql
@@ -0,0 +1,14 @@
+{% snapshot customer_snapshot %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='customer_id',
+      strategy='timestamp',
+      updated_at='updated_at'
+    )
+}}
+
+SELECT * FROM customers
+
+{% endsnapshot %}
